4f97aea644e12eff46eeb7492e890bd358304b45
ehsan <ahmednoha771@gmail.com>
2023-06-20 19:16:17 +0300

    adding splash screen

--- app/src/main/java/com/example/skysiren/MainActivity.kt
+++ app/src/main/java/com/example/skysiren/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.skysiren
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.airbnb.lottie.LottieAnimationView
+import com.example.skysiren.HomeActivity.HomeActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Handler().postDelayed({
+
+            intent = Intent(this@MainActivity, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }, 5000)
     }
 }
